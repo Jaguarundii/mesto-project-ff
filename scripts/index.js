b0b828d7951a9507ba22da03a683a0be40cbeec0
Sopha <130227772+Jaguarundii@users.noreply.github.com>
2024-03-18 15:19:17 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
+const cardContainer = document.querySelector('.places__list');
+const templateElement = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
+function addCard(item){
+    cardContainer.append(item);
+}
+
 // @todo: Функция создания карточки
+
+function createCard(item,deleteCard){
+    const cardElement = templateElement.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
+
+    const deleteCardButton = cardElement.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click',deleteCard);
+    
+    return cardElement;
+
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(event){
+    const cardRemove = event.target.closest('.card');
+    cardRemove.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(item){
+    const createdCard = createCard(item,deleteCard);
+    addCard(createdCard);
